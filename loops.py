f55cb48cfed7b2c7e051aaa68d13fb6c114ac3bd
geronimoooooooo <geronimoooooooo@gmail.com>
2018-04-29 02:53:58 +0200

    Create loops.py

---
+++ loops.py
@@ -0,0 +1,9 @@
+friends = ['john', 'pat', 'gary', 'michael']
+for i, name in enumerate(friends):
+    print "iteration {iteration} is {name}".format(iteration=i, name=name)
+    
+iteration 0 is john                                                                                                                                                                                                                              
+iteration 1 is pat                                                                                                                                                                                                                               
+iteration 2 is gary                                                                                                                                                                                                                              
+iteration 3 is michael  
+------------------------------------------
