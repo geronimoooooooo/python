c17f5d30afb7b1415734bf18aa3a114834341bc3
geronimoooooooo <geronimoooooooo@gmail.com>
2018-04-29 03:14:53 +0200

    Update loops.py

--- loops.py
+++ loops.py
@@ -7,3 +7,14 @@ iteration 1 is pat
 iteration 2 is gary                                                                                                                                                                                                                              
 iteration 3 is michael  
 ------------------------------------------
+activities = {8: 'Sleeping',
+              9: 'Commuting',
+              17: 'Working',
+              18: 'Commuting',
+              20: 'Eating',
+              22: 'Resting' }
+print activities
+print activities[8]
+for keys in activities.keys():
+    print "%s: %s"  %(keys, activities[keys] )
+------------------------------------------
