c228bb5c57c437ba3fa30d7b3c880cc7283ee869
geronimoooooooo <geronimoooooooo@gmail.com>
2018-04-29 03:14:29 +0200

    Create time.py

---
+++ time.py
@@ -0,0 +1,5 @@
+from time import localtime
+
+print localtime()
+time.struct_time(tm_year=2018, tm_mon=4, tm_mday=29, tm_hour=1, tm_min=4, tm_sec=41, tm_wday=6, tm_yday=119, tm_isdst=0)    
+--------------------------------------------------------------------
