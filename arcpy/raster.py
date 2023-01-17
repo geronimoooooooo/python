75505cff9cb5829deb1fbf53f91daf09fb253944
geronimoooooooo <geronimoooooooo@gmail.com>
2023-01-17 07:15:31 +0100

    Update raster.py

--- arcpy/raster.py
+++ arcpy/raster.py
@@ -1,3 +1,5 @@
 What is the Raster Cell Iterator? https://pro.arcgis.com/en/pro-app/latest/help/analysis/spatial-analyst/raster-cell-iterator/what-is-the-raster-cell-iterator.htm
 Introducing the Raster Cell Iterator https://www.esri.com/arcgis-blog/products/spatial-analyst/analytics/introducing-the-raster-cell-iterator/
+  
+  Get Raster Properties (Data Management) https://pro.arcgis.com/en/pro-app/latest/tool-reference/data-management/get-raster-properties.htm
 
