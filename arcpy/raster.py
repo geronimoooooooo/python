1fca78266113793daeba1a6a7d528d4229b7bc87
geronimoooooooo <geronimoooooooo@gmail.com>
2023-01-20 05:45:23 +0100

    Update raster.py

--- arcpy/raster.py
+++ arcpy/raster.py
@@ -2,4 +2,16 @@ What is the Raster Cell Iterator? https://pro.arcgis.com/en/pro-app/latest/help/
 Introducing the Raster Cell Iterator https://www.esri.com/arcgis-blog/products/spatial-analyst/analytics/introducing-the-raster-cell-iterator/
   
   Get Raster Properties (Data Management) https://pro.arcgis.com/en/pro-app/latest/tool-reference/data-management/get-raster-properties.htm
+    
+    
+Raster mergen und Con(): https://pro.arcgis.com/en/pro-app/latest/help/analysis/spatial-analyst/mapalgebra/building-complex-statements.htm
+Raster modifien und abspeichern, for: https://support.esri.com/en/technical-article/000022418
+Additional ArcGIS Pro Functions: https://gsp.humboldt.edu/olm/Courses/GSP_318/05_4_1_RasterArithmatic.html
+    
+   
+numpy, copyRaster: https://gist.github.com/kevin-peel/e47738b6afba31f5419c
+Raster Compare: https://pro.arcgis.com/en/pro-app/latest/tool-reference/data-management/raster-compare.htm
+map algebra on raster: https://www.e-education.psu.edu/geog485/node/311
+    
+SINNVOLLE FOLIEN MIT CODE SNIPPETS zu Rastern: https://here.isnew.info/accessing-raster-data-using-arcpy.html#/-save-raster
 
