d11fb4c4c74a6e0adf11ede32e8a97af399d267d
geronimoooooooo <geronimoooooooo@gmail.com>
2023-03-07 04:43:05 +0100

    Update AAA_Python_how_to.py

--- AAA_Python_how_to.py
+++ AAA_Python_how_to.py
@@ -1,3 +1,13 @@
 
-How to sort dictionary by key in numerical order Python
+How to sort dictionary by key or value in numerical order Python
+https://realpython.com/sort-python-dictionary/
 https://stackoverflow.com/questions/22264956/how-to-sort-dictionary-by-key-in-numerical-order-python
+https://stackoverflow.com/questions/9001509/how-do-i-sort-a-dictionary-by-key
+https://stackoverflow.com/questions/613183/how-do-i-sort-a-dictionary-by-value
+  
+Merge 2 dict
+https://www.digitalocean.com/community/tutorials/python-add-to-dictionary#adding-to-a-dictionary-using-the-merge-operator
+  
+  
+
+
